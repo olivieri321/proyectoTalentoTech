928da6ce5eb2a417024a5cbb8d98771bbdc46d80
Facundo <83927968+olivieri321@users.noreply.github.com>
2024-12-27 20:24:49 -0300

    update Reseñas

--- scripts/scriptsContacto.js
+++ scripts/scriptsContacto.js
@@ -1,20 +1,31 @@
-
 let obtuboTarjetas = false;
 
-try{
+try {
     let contenedorTarjetas = document.querySelector("#reseñas");
     let tarjetas = document.querySelectorAll(".tarjetaReseña");
+    obtuboTarjetas = true;
     let indiceRotacion = 0;
-    let desplazamiento = tarjetas[0].offsetWidth + 20 ; 
+    let desplazamiento = tarjetas[0].offsetWidth + 20;
 
     function rotarTarjetas() {
+        contenedorTarjetas.style.transition = "transform 4.0s ease-in-out";
         contenedorTarjetas.style.transform = `translateX(-${desplazamiento * indiceRotacion}px)`;
-        indiceRotacion = (indiceRotacion + 1) % tarjetas.length;
+        indiceRotacion = (indiceRotacion + 1) % (tarjetas.length - 2);
+
+        if (indiceRotacion === 0) {
+            setTimeout(() => {
+                contenedorTarjetas.style.transition = "none";
+                contenedorTarjetas.style.transform = `translateX(0px)`;
+            }, 500);
+        }
     }
+    rotarTarjetas();
+    setInterval(rotarTarjetas, 4000);
 
-    setInterval(rotarTarjetas, 2000); 
-
-}catch(error){
-    console.log("Error al mover las tarjetas de reseña, probablemente causado por cambio de visibilidad" + error)
+} catch (error) {
+    if (!obtuboTarjetas) {
+        console.log("Error al obtener tarjetas de reseña " + error);
+    } else {
+        console.log("Error al mover las tarjetas de reseña, probablemente causado por cambio de visibilidad: " + error);
+    }
 }
- 
